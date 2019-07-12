98f21fd19e0819e29823bb76646ba5eb5a11d6de
somanath9094 <36064444+somanath9094@users.noreply.github.com>
2019-07-12 12:05:05 +0530

    Create fourth.py

---
+++ fourth.py
@@ -0,0 +1,6 @@
+list_A=["a","e","i","o","u","b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+alpb=str(input("enter your string"))
+if(alpb in list_A):
+  print("Alphabet")
+else:
+  print("No")
