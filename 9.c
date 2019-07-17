604812c1b5947c0302066c747abb0da691f92eb9
somanath9094 <36064444+somanath9094@users.noreply.github.com>
2019-07-17 11:06:08 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,i,k;
+  printf("enter the value of n :");
+  scanf("%d",&n);
+  printf("enter the value of k :");
+  scanf("%d",&k);
+  for(i=1;i<=k;i++)
+  {
+    printf("%d",i);
+  }
+}
